1223b1fbc4a28d62d87ec173ef61027cf5c790cb
ThanhTuanIT <nguyenthanhtuan.15it@gmail.com>
2021-02-28 21:52:33 +0700

    l2.1

--- js/upload.js
+++ js/upload.js
@@ -5,7 +5,6 @@ function readUrl(input) {
         let imgData = e.target.result;
         let imgName = input.files[0].name;
         input.setAttribute("data-title", imgName);
-        console.log(e.target.result);
       }
       reader.readAsDataURL(input.files[0]);
     }
